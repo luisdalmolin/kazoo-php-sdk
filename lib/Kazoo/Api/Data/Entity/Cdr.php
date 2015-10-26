6a61019815e26c47a1f23c07bfb30fdf2252d0d1
fogeytron <fogeytron@gmail.com>
2015-10-26 11:59:10 -0400

    Change when cdrs pull the full record

--- lib/Kazoo/Api/Data/Entity/Cdr.php
+++ lib/Kazoo/Api/Data/Entity/Cdr.php
@@ -12,5 +12,35 @@ class Cdr extends AbstractEntity {
     public function initDefaultValues() {
         
     }
+    
+    /**
+     * 
+     * @param type $prop
+     * @return type
+     */
+    public function __get($prop) {
+        $return = null;
+        switch ($this->_state) {
+            case self::STATE_NEW:
+            case self::STATE_PARTIAL_HYDRATED:
+                $pk = self::DOC_KEY;
+                
+                if (property_exists($this->_data, $prop)) {
+                    $return = $this->_data->$prop;
+                } else if (isset($this->$pk)) {
+                    $result = $this->_client->get($this->_uri, array());
+                    $this->updateFromResult($result->data);
+                    $return = $this->_data->$prop;
+                }
+                break;
+            case self::STATE_HYDRATED:
+                if (property_exists($this->_data, $prop)) {
+                    $return = $this->_data->$prop;
+                }
+                break;
+        }
+        
+        return $return;
+    }
 
 }
