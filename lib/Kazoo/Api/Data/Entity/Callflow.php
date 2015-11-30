e2a101c30ef59c39cc3abb849c7496c4862e0f9e
fogeytron <fogeytron@gmail.com>
2015-11-30 17:13:48 -0500

    Change Callflows to not hydrate all the time

--- lib/Kazoo/Api/Data/Entity/Callflow.php
+++ lib/Kazoo/Api/Data/Entity/Callflow.php
@@ -13,6 +13,36 @@ class Callflow extends AbstractEntity {
 
     protected static $_schema_name = "callflows.json";
     protected static $_callflow_module = "callflow";
+    
+    /**
+     * 
+     * @param type $prop
+     * @return type
+     */
+    public function __get($prop) {
+        $return = null;
+        switch ($this->_state) {
+            case self::STATE_NEW:
+            case self::STATE_PARTIAL_HYDRATED:
+                $pk = self::DOC_KEY;
+                 
+                if (property_exists($this->_data, $prop)) {
+                    $return = $this->_data->$prop;
+                } else if (isset($this->$pk)) {
+                    $result = $this->_client->get($this->_uri, array());
+                    $this->updateFromResult($result->data);
+                    $return = $this->_data->$prop;
+                }
+                break;
+            case self::STATE_HYDRATED:
+                if (property_exists($this->_data, $prop)) {
+                    $return = $this->_data->$prop;
+                }
+                break;
+        }
+         
+        return $return;
+    }
 
     public function initDefaultValues() {
         unset($this->featurecode);  //Dont set feature codes by default
